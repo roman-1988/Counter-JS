a7e248598a88e3b89379843aeda285b9cd1c1752
Roman <roman-1@inbox.ru>
2023-01-11 20:40:16 +0300

    В файле index.js создали три функции: handleIncrementButtonClick, handleDecrementButtonClick, handleResetButtonClick. В файле view.js в функции createView - создали три обработчика события по клику мыши с функциями: onIncrementButtonClick, onDecrementButtonClick, onResetButtonClick. В параметрах функции createView передали обьект с тремя этими функциями. В файле index.js в переменной view функции createView в параметры передали обьект с ключи которых функций on... а значения функции handle...

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const INITIAL_COUNTER = 0;
 
 const counter = createCounter(INITIAL_COUNTER);
-const view = createView();
-
-console.log(counter.get());
+const view = createView({
+    onIncrementButtonClick: handleIncrementButtonClick,
+    onDecrementButtonClick: handleDecrementButtonClick,
+    onResetButtonClick: handleResetButtonClick,
+});
 
 view.render({
     counter: counter.get()
 });
+
+function handleIncrementButtonClick() {
+    counter.increment();
+    view.render({
+        counter: counter.get()
+    });
+}
+
+function  handleDecrementButtonClick() {
+    counter.decrement();
+    view.render({
+        counter: counter.get()
+    });
+}
+
+function  handleResetButtonClick() {
+    counter.reset();
+    view.render({
+        counter: counter.get()
+    });
+}
 
--- view.js
+++ view.js
@@ -1,8 +1,24 @@
-function createView() {
+function createView({
+    onIncrementButtonClick,
+    onDecrementButtonClick,
+    onResetButtonClick,
+}) {
     const counterNode = document.querySelector(".js-counter");
     const incrementBtnNode = document.querySelector(".js-increment-btn");
     const decrementBtnNode = document.querySelector(".js-decrement-btn");
     const resetBtnNode = document.querySelector(".js-reset-btn");
+
+    incrementBtnNode.addEventListener("click", () => {
+        onIncrementButtonClick();
+    });
+
+    decrementBtnNode.addEventListener("click", () => {
+        onDecrementButtonClick();
+    });
+
+    resetBtnNode.addEventListener("click", () => {
+        onResetButtonClick();
+    });
 
     return {
         render({counter}) {
@@ -10,3 +26,4 @@ function createView() {
         }
     }
 }
+
